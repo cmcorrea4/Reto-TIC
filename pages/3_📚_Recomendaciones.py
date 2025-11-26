dc598931e52a90d35eef7cc8cc9d97c9687d45ca
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2025-11-26 10:02:11 -0500

    Update 3_📚_Recomendaciones.py

--- pages/3_📚_Recomendaciones.py
+++ pages/3_📚_Recomendaciones.py
@@ -7,7 +7,7 @@ import numpy as np
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-z
+
 
 # ================================================================
 # CONFIGURACIÓN
